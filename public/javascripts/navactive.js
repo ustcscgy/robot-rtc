b10d5058f407e061e4aaceb45844eac67100302d
jimmy <jimmy@nonexist.com>
2013-04-26 17:56:45 +0800

    auto active nav

---
+++ public/javascripts/navactive.js
@@ -0,0 +1,18 @@
+$(function () {
+    var url = window.location.href;
+    var menus = ['login', 'reg', 'logout'];
+    var i;
+    var str;
+    var flag = false;
+    for (i = 0; i < menus.length; i++){
+	str = menus[i];
+	if (url.indexOf(str) !== -1){
+	    $('.nav > li > a[href*="' + str + '"]')[0].parentNode.className = 'active';
+	    flag = true;
+	    break;
+	}
+    }
+    if (!flag){
+	$('.nav > li:first-child').addClass('active');
+    }
+});
